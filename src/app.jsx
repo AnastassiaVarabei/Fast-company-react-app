0ca74d07a18a5aa1ab54637cd7f54d96776565a9
Anastassia Varabei <nastenkaok1984@gmail.com>
2021-11-14 19:35:47 +0300

    added app.jsx

---
+++ src/app.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import Users from "./components/users";
+
+const App = () => {
+  return (
+    <div>
+      <main>
+        <Users />
+      </main>
+    </div>
+  );
+};
+
+export default App;
